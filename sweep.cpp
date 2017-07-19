bc686011477edc3cde9b64db678f00745aec824c
nrupprecht <nathaniel.rupprecht@gmail.com>
2017-07-19 15:24:55 -0400

    Small updates to sweep.

--- sweep.cpp
+++ sweep.cpp
@@ -5,9 +5,10 @@
 int main(int argc, char** argv) {
   // Parameters
   int width = 1000, height = 1000;
+  int size = -1;
+  int partition = 10, rep = 5;
   double minP = 0.55;
   double maxP = 0.65;
-  int partition = 10, rep = 5;
 
   // Seed rand
   srand48( std::time(0) );
@@ -15,6 +16,7 @@ int main(int argc, char** argv) {
   ArgParse parser(argc, argv);
   parser.get("width", width);
   parser.get("height", height);
+  parser.get("size", size);
   parser.get("maxP", maxP);
   parser.get("minP", minP);
   parser.get("partition", partition);
@@ -27,11 +29,16 @@ int main(int argc, char** argv) {
     exit(1);
   }
 
+  // Reset width and height
+  if (size>0) width = height = size;
+
   // Create a percolation
   Percolation percolation;
   percolation.setDoBMP(false);
   percolation.setDims(width, height);
 
+  // Timing
+  auto start = high_resolution_clock::now();
   // Store data
   vector<pair<double, double> > maxPercent, aveCluster, aveFiniteCluster;
   // Do trials
@@ -51,8 +58,17 @@ int main(int argc, char** argv) {
     aveCluster.push_back(pair<double, double>(prob, ave));
     aveFiniteCluster.push_back(pair<double, double>(prob, aveF));
   }
+  // Timing
+  auto end = high_resolution_clock::now();
+  duration<double> span = duration_cast<duration<double>>(end-start);
+  double time = span.count();
 
   // Print data
+  cout << "Partitions:  " << partition << "\n";
+  cout << "Repetitions: " << rep << "\n";
+  cout << "Dimensions:  " << width << "x" << height << "\n";
+  cout << "Time:        " << time << "\n";
+  cout << endl;
   cout << "maxCluster=" << maxPercent << ";\n";
   cout << "aveCluster=" << aveCluster << ";\n";
   cout << "aveFiniteCluster=" << aveFiniteCluster << ";\n";
